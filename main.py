b9371504d4acfbbbe74d2dcf12818ffdcccff33f
Sajim <the8bitcrow@gmail.com>
2025-05-05 22:36:17 +0600

    added more words

--- main.py
+++ main.py
@@ -10,10 +10,13 @@ word1 = [
     "brooms", "sandals", "mirrors", "tacos", "drums", "mittens", "candles", "scooters", "beans", "sponges",
     "grapes", "eyebrows", "drawers", "lollipops", "kettles", "hats", "goggles", "flosses", "pebbles", "shoes",
     "apple", "mountain", "river", "cat", "book", "lamp", "cloud", "pencil", "shoe", "ocean", "robot", "bicycle",
-    "garden", "mirror", "clock", "tree", "train", "window", "camera", "dragon", "backpack", "planet", "key", "feather", 
-    "pizza", "umbrella", "jungle", "pillow", "castle", "star", "rocket", "carrot", "volcano", "coin", "bridge", "island", 
-    "violin", "button", "lighthouse", "helmet", "suitcase", "banana", "glove", "ghost", "tower", "cup", "door", "chair", 
-    "magnet", "sandwich"
+    "gardens", "mirrors", "clocks", "trees", "trains", "windows", "cameras", "dragons", "backpacks", "planets", "keys", "feathers", 
+    "pizzas", "umbrellas", "jungles", "pillows", "castles", "stars", "rockets", "carrots", "volcanos", "coins", "bridges", "islands", 
+    "violins", "buttons", "lighthouses", "helmets", "suitcases", "bananas", "gloves", "ghosts", "towers", "cups", "doors", "chairs", 
+    "magnets", "sandwichs", "orphans", "police", "little boys", "little girls", "caves", "cucumbers", "alchohol", "asians",
+    "africans", "americans", "porn", "singers", "politicians", "women", "bottles", "animals", "plastics", "oceans", "clowns",
+    "car salesmen", "students", "friends", "tests", "bathrooms", "hospitals", "organ donors", "live streamers", "Vtubers",
+    "white vans"
 ]
 
 word2 = [
@@ -22,7 +25,15 @@ word2 = [
     "squished", "stuck", "soaked", "flattened", "creased", "bruised", "stained", "lost", "drained", "crumpled",
     "glued", "dented", "torn", "bent", "knitted", "wrapped", "stretched", "covered", "plastered", "inflated",
     "deflated", "chewed", "boiled", "flipped", "twirled", "hacked", "erased", "snapped", "drenched", "splattered",
-    "gambled", "lost", "yeeted", "coped", "flipped"
+    "gambled", "lost", "yeeted", "coped", "flipped", "incerted", "penetrated", " vored", "enjoyed", "sizzled", "ringed",
+    "burped", "vomited", " snorted", "buried", "full of cats", "underground", "in the air", "inverted", "pumped",
+    "scamed", "perfected", "are a failure", "orphaned", "need some milk", "are underachievers", "are stinky",
+    "has a foot fetish", "has an armpit fetish", "doesnt know directions", "are bilingual", "got arrested", "sad :(",
+    "are wet", "want to be wet", "in your area", "abandoned", "sold", "got sequels", "polluted", "groomed", "diddied",
+    "in a coma", "want to pee", "are peeing", "are full of piss", "never saw a bird", "doesnt exist", "cant spell", 
+    "are perverts", "are horny", "hate purple", "are poor", "have AIDS", "have stds", "doesnt know what sex is",
+    "love incest", "have incest", "are siscons", "are brocons", "are lolicons", "aborted", "are forever gone",
+    "are funny"
 ]
 
 @app.route("/", methods=["GET", "POST"])
